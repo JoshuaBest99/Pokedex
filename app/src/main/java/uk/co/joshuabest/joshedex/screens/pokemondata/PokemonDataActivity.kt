cc800e932b288cf424478cbed947391ad9cfa15f
Josh <Joshuabest99@hotmail.com>
2020-09-07 20:29:53 +0100

    Created data class

--- app/src/main/java/uk/co/joshuabest/joshedex/screens/pokemondata/PokemonDataActivity.kt
+++ app/src/main/java/uk/co/joshuabest/joshedex/screens/pokemondata/PokemonDataActivity.kt
@@ -1,6 +1,18 @@
 package uk.co.joshuabest.joshedex.screens.pokemondata
 
+import androidx.lifecycle.ViewModelProviders
+import uk.co.joshuabest.joshedex.R
 import uk.co.joshuabest.joshedex.util.JoshedexActivity
 
 class PokemonDataActivity : JoshedexActivity(){
+
+    lateinit var viewModel: PokemonDataViewModel
+
+    override fun provideLayoutId(): Int = R.layout.activity_pokemon_data
+
+    override fun observeViewModel() {
+        viewModel = ViewModelProviders.of(this).get(PokemonDataViewModel::class.java)
+    }
+
+
 }
